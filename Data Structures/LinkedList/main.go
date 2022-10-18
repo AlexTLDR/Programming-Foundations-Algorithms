dbdfe33c7ea680742987a78d1b4059093584c9ad
Alex <abadragan@gmail.com>
2022-10-18 10:29:21 +0200

    linked list initial commit

---
+++ Data Structures/LinkedList/main.go
@@ -0,0 +1,18 @@
+package main
+
+type node struct {
+	data int
+	next *node
+}
+
+type linkedList struct {
+	lenght int
+	head   *node
+	tail   *node
+}
+
+func main() {
+
+	node := &node{data: 20}
+
+}
