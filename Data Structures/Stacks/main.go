c6f926269d1415112c48f526ad69d83dc845afb6
Alex <abadragan@gmail.com>
2022-10-19 11:11:59 +0200

    added Pop method

--- Data Structures/Stacks/main.go
+++ Data Structures/Stacks/main.go
@@ -13,6 +13,8 @@ func main() {
 	s.Push(3)
 	s.Push(4)
 	fmt.Printf("%v\n", *s)
+	s.Pop()
+	fmt.Printf("stack after Pop method: %v\n", *s)
 }
 
 //newStack returns a new stack
@@ -27,9 +29,21 @@ func (s *stack) isEmpty() bool {
 	return len(s.count) == 0
 }
 
-//Push adds a node to the stack
+//Push adds an element to the stack
 
 func (s *stack) Push(n int) *stack {
 	s.count = append(s.count, n)
 	return s
+}
+
+//Pop removes the last element from the stack and returns the rest of the elements
+
+func (s *stack) Pop() (int, error) {
+	if len(s.count) == 0 {
+		return 0, fmt.Errorf("empty stack")
+	}
+	fmt.Println(s.count[len(s.count)-1]) //print out the removed element
+	element := s.count[len(s.count)-1]
+	s.count = s.count[:len(s.count)-1]
+	return element, nil
 }
