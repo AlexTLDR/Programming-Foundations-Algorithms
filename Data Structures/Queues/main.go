637d6098aa9f677012ec7323e76432d91386ce21
Alex <abadragan@gmail.com>
2022-10-19 11:45:18 +0200

    added dequeue method

--- Data Structures/Queues/main.go
+++ Data Structures/Queues/main.go
@@ -13,6 +13,8 @@ func main() {
 	q.add(3)
 	q.add(4)
 	fmt.Println(*q)
+	q.remove()
+	fmt.Println(*q)
 }
 
 //Creating the Queue based on the queue struct
@@ -28,4 +30,22 @@ func createQueue() *queue {
 func (q *queue) add(n int) *queue {
 	q.values = append(q.values, n)
 	return q
+}
+
+//Check if the Queue is empty
+
+func (q *queue) isEmpty() bool {
+	return len(q.values) == 0
+}
+
+//Remove the first element from the queue -> dequeue
+
+func (q *queue) remove() (int, error) {
+	if len(q.values) == 0 {
+		return 0, fmt.Errorf("empty queue")
+	}
+	element := q.values[0]
+	fmt.Println(element)
+	q.values = q.values[1:]
+	return element, nil
 }
