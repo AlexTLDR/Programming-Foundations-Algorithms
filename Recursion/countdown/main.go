4319da5d0a1916ccf497e08b394868cbf0aa1bba
Alex <abadragan@gmail.com>
2022-10-19 16:22:32 +0200

    added comment to the code

--- Recursion/countdown/main.go
+++ Recursion/countdown/main.go
@@ -12,5 +12,19 @@ func countdown(x int) {
 	} else {
 		fmt.Println(x, "...")
 		countdown(x - 1)
+		fmt.Println("foo")
+		/* the foo will get printed x times as it is part of the countdown function
+		5 ...
+		4 ...
+		3 ...
+		2 ...
+		1 ...
+		Done!
+		foo
+		foo
+		foo
+		foo
+		foo
+		*/
 	}
 }
