5949f2589941662a02f95d4485d52c3d442ace68
Alex <abadragan@gmail.com>
2022-10-20 13:30:10 +0200

    migrating to 2 functions

--- Sorting Data/The merge sort/main.go
+++ Sorting Data/The merge sort/main.go
@@ -9,7 +9,7 @@ func main() {
 	fmt.Println("sorted:", items)
 }
 
-func mergeSort(dataset []int) {
+func mergeSort(dataset []int) []int {
 	if len(dataset) > 1 {
 		mid := len(dataset) / 2
 		leftarr := dataset[:mid]
@@ -18,41 +18,45 @@ func mergeSort(dataset []int) {
 		// recursively break down the arrays
 		mergeSort(leftarr)
 		mergeSort(rightarr)
+		return merge(leftarr, rightarr)
 
-		// perform the merging
+	
+}
 
-		i := 0 // index into the left array
-		j := 0 // index into the right array
-		k := 0 // index into the merged array
+func merge(leftarr, rightarr []int) []int {
+	// perform the merging
 
-		// while both arrays have content
+	i := 0 // index into the left array
+	j := 0 // index into the right array
+	k := 0 // index into the merged array
 
-		for i < len(leftarr) && j < len(rightarr) {
-			if leftarr[i] < rightarr[j] {
-				dataset[k] = leftarr[i]
-				i += 1
-			} else {
-				dataset[k] = rightarr[j]
-				j += 1
-				k += 1
-			}
-		}
+	// while both arrays have content
 
-		// if the left array still has values, add them
-
-		for i < len(leftarr) {
+	for i < len(leftarr) && j < len(rightarr) {
+		if leftarr[i] < rightarr[j] {
 			dataset[k] = leftarr[i]
 			i += 1
-			k += 1
-		}
-
-		// if the right array still has values, add them
-
-		for j < len(rightarr) {
+		} else {
 			dataset[k] = rightarr[j]
 			j += 1
 			k += 1
 		}
-
 	}
+
+	// if the left array still has values, add them
+
+	for i < len(leftarr) {
+		dataset[k] = leftarr[i]
+		i += 1
+		k += 1
+	}
+
+	// if the right array still has values, add them
+
+	for j < len(rightarr) {
+		dataset[k] = rightarr[j]
+		j += 1
+		k += 1
+	}
+
 }
