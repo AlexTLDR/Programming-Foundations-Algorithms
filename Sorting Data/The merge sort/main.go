bbe8747e334502b81f83d7560553a9e4d78bea3b
Alex <abadragan@gmail.com>
2022-10-20 10:51:24 +0200

    merge sort initial commit

---
+++ Sorting Data/The merge sort/main.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+	items := []int{6, 20, 8, 19, 56, 23, 87, 41, 49, 53}
+}
+
+func mergeSort(dataset []int) {
+	mid := len(dataset) / 2
+	leftarr := dataset[:mid]
+	rightarr := dataset[mid:]
+
+}
