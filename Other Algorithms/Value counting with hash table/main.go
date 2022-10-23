ca438cb03e9a2b3f7c0b282b50b33d143d214b5a
Alex <abadragan@gmail.com>
2022-10-23 18:01:46 +0200

    added comments

--- Other Algorithms/Value counting with hash table/main.go
+++ Other Algorithms/Value counting with hash table/main.go
@@ -10,7 +10,13 @@ func main() {
 }
 
 func printUniqueValue(items []string) {
+
+	// create a hashtable object to hold the items and counts
+
 	dict := make(map[string]int)
+
+	//iterate over each item and increment the count for each one
+
 	for _, num := range items {
 		dict[num] = dict[num] + 1
 	}
