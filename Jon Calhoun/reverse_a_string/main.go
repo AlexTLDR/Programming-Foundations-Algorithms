d9e35c57483ada5c2fe58828df99f42dcf22d423
Alex <abadragan@gmail.com>
2022-10-23 21:30:07 +0200

    passed the test

--- Jon Calhoun/reverse_a_string/main.go
+++ Jon Calhoun/reverse_a_string/main.go
@@ -4,10 +4,10 @@ import "fmt"
 
 func main() {
 	word := "cat"
-	fmt.Println(reverse(word))
+	fmt.Println(Reverse(word))
 }
 
-func reverse(word string) string {
+func Reverse(word string) string {
 	var reversed string
 	for i := len(word) - 1; i >= 0; i-- {
 		reversed = reversed + string(word[i])
