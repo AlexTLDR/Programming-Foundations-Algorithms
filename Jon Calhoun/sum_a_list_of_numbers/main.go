8fce455f0f63407f66188f870cfa616ff2722c2d
Alex <abadragan@gmail.com>
2022-10-23 21:04:36 +0200

    adde recursive solution + test

--- Jon Calhoun/sum_a_list_of_numbers/main.go
+++ Jon Calhoun/sum_a_list_of_numbers/main.go
@@ -3,8 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	numbers := []int{}
+	numbers := []int{1, 2, 3, 4, 5, 6}
 	fmt.Println(Sum(numbers))
+	fmt.Println(SumRecursive(numbers))
 }
 
 func Sum(numbers []int) int {
@@ -14,4 +15,13 @@ func Sum(numbers []int) int {
 		sum += v
 	}
 	return sum
+}
+
+func SumRecursive(numbers []int) int {
+
+	if len(numbers) == 0 {
+		return 0
+	}
+
+	return numbers[0] + SumRecursive(numbers[1:])
 }
