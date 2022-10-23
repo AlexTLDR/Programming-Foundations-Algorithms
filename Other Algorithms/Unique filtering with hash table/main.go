dc7c05f3923fe1d56722881cd137141ba5488b39
Alex <abadragan@gmail.com>
2022-10-23 17:35:08 +0200

    corrected the comments

--- Other Algorithms/Unique filtering with hash table/main.go
+++ Other Algorithms/Unique filtering with hash table/main.go
@@ -6,22 +6,16 @@ func main() {
 	items := []string{"apple", "pear", "orange", "banana", "apple", "orange", "apple", "pear", "banana",
 		"orange", "apple", "kiwi", "pear", "apple", "orange"}
 
-	//create a hashtable
-
-	// filter := make(map[string]int)
-
-	// for _, s := range items {
-	// 	filter[s]++
-	// }
-
-	// fmt.Println(filter)
-
 	fmt.Println(removeDuplicate(items))
 }
 
 func removeDuplicate(items []string) []string {
+
+	// create a hashtable
 	filter := make(map[string]bool)
 	list := []string{}
+
+	//loop over each item
 	for _, item := range items {
 		if _, value := filter[item]; !value {
 			filter[item] = true
