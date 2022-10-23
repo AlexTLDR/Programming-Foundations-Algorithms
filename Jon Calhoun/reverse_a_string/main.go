a0a30940a0c00daf9e441f3d6c43b995158a0714
Alex <abadragan@gmail.com>
2022-10-23 21:39:29 +0200

    added BuildStrReversed + test pass

--- Jon Calhoun/reverse_a_string/main.go
+++ Jon Calhoun/reverse_a_string/main.go
@@ -5,12 +5,22 @@ import "fmt"
 func main() {
 	word := "cat"
 	fmt.Println(Reverse(word))
+	fmt.Println(BuildStrReversed(word))
 }
 
 func Reverse(word string) string {
 	var reversed string
 	for i := len(word) - 1; i >= 0; i-- {
 		reversed = reversed + string(word[i])
+	}
+
+	return reversed
+}
+
+func BuildStrReversed(word string) string {
+	var reversed string
+	for i := range word {
+		reversed = string(word[i]) + reversed
 	}
 
 	return reversed
