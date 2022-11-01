090ce4986270eeb6e1ef1ee81429f23b0dd0ac15
Alex <abadragan@gmail.com>
2022-11-01 15:04:41 +0100

    implemented for loop

--- Jon Calhoun/factor_a_number/main.go
+++ Jon Calhoun/factor_a_number/main.go
@@ -1,9 +1,21 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	primes := []int{2, 3, 5, 7}
+	number := 30
+	fmt.Println(Factor(primes, number))
 }
 
 func Factor(primes []int, number int) []int {
-	return nil
+	var res []int
+	for _, prime := range primes {
+		if number%prime == 0 {
+			res = append(res, prime)
+			number = number / prime
+		}
+	}
+
+	return res
 }
