2960fef696c7f2512169a1e2dc5dbd212d7af4a2
Alex <abadragan@gmail.com>
2022-11-01 14:37:49 +0100

    implemented nested loop

--- Jon Calhoun/find_two_numbers_that_sum_to_a_given_amount/main.go
+++ Jon Calhoun/find_two_numbers_that_sum_to_a_given_amount/main.go
@@ -1,9 +1,24 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	numbers := []int{1, 2, 3, 4}
+	sum := 7
+
+	fmt.Println(FindTwoThatSum(numbers, sum))
 }
 
 func FindTwoThatSum(numbers []int, sum int) (int, int) {
-	return 0, 0
+	for i, v := range numbers {
+		for j, v2 := range numbers {
+			if i == j {
+				continue
+			}
+			if v+v2 == sum {
+				return i, j
+			}
+		}
+	}
+	return -1, -1
 }
