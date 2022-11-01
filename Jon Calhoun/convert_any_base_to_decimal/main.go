43a56f8406e3e9f3a7e0f92fe413bab048f3b155
Alex <abadragan@gmail.com>
2022-11-01 13:02:35 +0100

    working on for loop

--- Jon Calhoun/convert_any_base_to_decimal/main.go
+++ Jon Calhoun/convert_any_base_to_decimal/main.go
@@ -1,9 +1,22 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func main() {
+	value := "1110"
+	base := 2
+
+	fmt.Println(BaseToDec(value, base))
 }
 
 func BaseToDec(value string, base int) int {
-	return 0
+	//i, _ := strconv.Atoi(value)
+	result := 0
+	for i := 0; i < len(value); i++ {
+		digit := base ^ len(value) - 1
+		result += digit
+	}
+	return result
 }
