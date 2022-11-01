eb304b532075a9d1f4362ef7eb1841d47a571d81
Alex <abadragan@gmail.com>
2022-11-01 13:31:43 +0100

    added BaseToDec & DecToBase

--- Jon Calhoun/convert_any_base_to_any_other_base/main.go
+++ Jon Calhoun/convert_any_base_to_any_other_base/main.go
@@ -1,9 +1,34 @@
 package main
 
+import "fmt"
+
 func main() {
 
 }
 
 func BaseToBase(value string, base, newBase int) string {
 	return ""
+}
+
+func BaseToDec(value string, base int) int {
+	result := 0
+	multiplier := 1
+	var val int
+	for i := len(value) - 1; i >= 0; i-- {
+		fmt.Sscanf(string(value[i]), "%X", &val)
+		result = result + multiplier*val
+		multiplier = multiplier * base
+	}
+	return result
+}
+
+func DecToBase(dec, base int) string {
+	var res string
+	for dec > 0 {
+		rem := dec % base
+		res = fmt.Sprintf("%X%s", rem, res)
+		dec = dec / base
+	}
+	return res
+
 }
