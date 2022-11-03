68582da7e26a407fc8c064c5a2fc9708adbfda8c
Alex <abadragan@gmail.com>
2022-11-03 13:42:21 +0100

    added swapped bool for code optimization

--- Jon Calhoun/bubble_sort/main.go
+++ Jon Calhoun/bubble_sort/main.go
@@ -12,11 +12,16 @@ func main() {
 
 func BubbleSortInt(list []int) {
 	for i := 0; i < len(list)-1; i++ {
+		swapped := false
 		for j := 1; j < i; j++ {
 			if list[j-1] > list[j] {
-				//swap
+				//swap - using the swapped bool to stop the loop if no swapping is necessary for optimization
 				list[j-1], list[j] = list[j], list[j-1]
+				swapped = true
 			}
+		}
+		if !swapped {
+			break
 		}
 	}
 }
