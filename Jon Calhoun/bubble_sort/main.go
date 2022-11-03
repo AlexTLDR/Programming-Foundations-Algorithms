b2acc2b68274039c1267af5ef6f37cf125efad9e
Alex <abadragan@gmail.com>
2022-11-03 13:36:06 +0100

    bubble sort implemented

--- Jon Calhoun/bubble_sort/main.go
+++ Jon Calhoun/bubble_sort/main.go
@@ -1,9 +1,22 @@
 package main
 
+import "fmt"
+
 func main() {
+
+	list := []int{78, 34, 98, 10, 17}
+	BubbleSortInt(list)
+	fmt.Println(list)
 
 }
 
 func BubbleSortInt(list []int) {
-
+	for i := 0; i < len(list)-1; i++ {
+		for j := 1; j < i; j++ {
+			if list[j-1] > list[j] {
+				//swap
+				list[j-1], list[j] = list[j], list[j-1]
+			}
+		}
+	}
 }
